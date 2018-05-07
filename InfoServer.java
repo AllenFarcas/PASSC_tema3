9655e38b91ad9aeda48690c64687c65b8a2396b0
AllenPianoMan <allenn_farcas@yahoo.com>
2018-05-07 23:09:44 +0300

    Add files via upload

---
+++ InfoServer.java
@@ -0,0 +1,124 @@
+
+import RequestReply.*;
+import MessageMarshaller.*;
+import Registry.*;
+import Commons.Address;
+
+
+interface Info {
+	int get_temp(String city);
+	String get_road_info(int road_ID);
+}
+
+class InfoImpl implements Info {
+	@Override
+	public int get_temp(String city) {
+		System.out.println("get_temp method is executing");
+		return 20;
+	}
+
+	@Override
+	public String get_road_info(int road_ID) {
+		System.out.println("get_road_info method is executing");
+		switch (road_ID) {
+			case 1: return "Blocat";
+			case 2: return "Liber";
+			case 3: return "Aglomerat";
+			default : return "Eroare";
+		}
+	}
+}
+
+class ServerTransformer implements ByteStreamTransformer {
+    private MessageServer originalServer;
+    public ServerTransformer(MessageServer s)
+    {
+	originalServer = s;
+    }
+    public byte[] transform(byte[] in) throws Exception{
+		Message msg;
+		Marshaller m = new Marshaller();
+		msg = m.unmarshal(in);
+		Message answer = originalServer.get_answer(msg);
+		byte[] bytes = m.marshal(answer);
+		return bytes;
+    }
+}
+
+class MessageServer {
+    public Message get_answer(Message msg) throws Exception{
+    	if(msg.sender.equals("InfoClientProxy")) {
+			InfoImpl info = new InfoImpl();
+			System.out.println("InfoServerProxy analyzing data");
+			String [] arrOfStr = msg.data.split(":", 5);
+			String parameters = arrOfStr[0];
+			String opcode = arrOfStr[1];
+			int opnum = Integer.parseInt(opcode);
+			String [] arrParam = parameters.split(" ", 5);
+			String parameter = arrParam[0];
+			switch (opnum){
+				//get_temp 0
+				case 0: {
+					System.out.println("InfoServerProxy: get_temp method");
+					java.lang.String $param_String_1 = parameter;
+					int $result;
+					$result = info.get_temp($param_String_1);
+					String dataResult = String.valueOf($result);
+					System.out.println("InfoServerProxy: result is " + dataResult);
+					Message answer = new Message("InfoServerProxy",dataResult);
+					return answer;
+				}
+				//get_road_info 1
+				case 1: {
+					System.out.println("InfoServerProxy: get_road_info method");
+					int $param_int_1 = Integer.parseInt(parameter);
+					String $result;
+					$result = info.get_road_info($param_int_1);
+					System.out.println("InfoServerProxy: result is " + $result);
+					Message answer = new Message("InfoServerProxy",$result);
+					return answer;
+				}
+			}
+		} else {
+    		System.out.println("InfoServerProxy Error: Somebody else is trying to communicate with me! ( "+msg.sender+" )");
+    		throw new Exception("Error: Somebody else is trying to communicate with me! ( "+msg.sender+" )");
+		}
+    	Message answer = new Message("InfoServer","Error");
+
+		return answer;
+    }
+}
+
+public class InfoServer {
+    public static void main(String args[]) {
+		InfoImpl infoImpl = new InfoImpl();
+		System.out.println("InfoServer main started");
+		try {
+			NamingService.registerMethod("MyInfoImpl",infoImpl);
+		} catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+    }
+}
+
+class InfoServerProxy {
+	private int portNumber;
+
+	public InfoServerProxy(int portNumber) {
+		this.portNumber = portNumber;
+	}
+
+	public void dispatch() {
+		Address myAddr = new Entry("127.0.0.1",portNumber);
+		Replyer rep = new Replyer("InfoServerProxy", myAddr);
+		//InfoImpl info = new InfoImpl();
+		ByteStreamTransformer transformer = new ServerTransformer(new MessageServer());
+		while(true) {
+			try {
+				rep.receive_transform_and_send_feedback(transformer);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+}
---
+++ NamingService.java
@@ -0,0 +1,103 @@
+import Commons.Address;
+import MessageMarshaller.Marshaller;
+import MessageMarshaller.Message;
+import Registry.*;
+import RequestReply.ByteStreamTransformer;
+import RequestReply.Replyer;
+import RequestReply.Requestor;
+
+class NamingServiceMessageServer extends MessageServer {
+	private int i=1112;
+	public Message get_answer(Message msg) throws Exception {
+		Message answer;
+		answer = new Message("NamingService", "Error");
+		if(msg.sender.equals("Server")) {
+			System.out.println("Naming service registering " + msg.data + " from " + msg.sender);
+			String [] arrOfStr = msg.data.split(":", 5);
+			String theDestName = arrOfStr[0];
+			String theDestObject = arrOfStr[1];
+			Entry createNewEntry = new Entry(NamingService.address,i);
+			i++;
+			Registry.instance().put(msg.data,createNewEntry);
+			Address addr = Registry.instance().get(msg.data);
+			if(addr == null) {
+				System.out.println("Naming service couldn't put " + theDestName + " in the database.");
+				//answer = new Message("Naming service", "Error putting data in database ");
+				throw new Exception("Error putting the data in the database.");
+			} else {
+				System.out.println("Naming service registered the object " + theDestObject +
+						" with the name "+ theDestName +" in the database.");
+				answer = new Message("Naming service", String.valueOf(addr.port()));
+			}
+		} else if (msg.sender.equals("Client")) {
+			System.out.println("Naming service is searching for " + msg.data + " in database.");
+			Address addr = Registry.instance().get(msg.data);
+			if(addr == null) {
+				System.out.println("Naming service didn't find " + msg.data + " in database.");
+				//answer = new Message("Naming service", "Error finding data in database ");
+				throw new Exception("Error finding data in database");
+			} else {
+				System.out.println("Naming service found " + msg.data + " in database.");
+				answer = new Message("Naming service", String.valueOf(addr.port()));
+				//answer = new Message("Addr.dest = "+addr.dest() + " addr.")
+			}
+		}
+		return answer;
+	}
+}
+
+public class NamingService {
+	public static final String address = "127.0.0.1";
+	public static final int portNo = 1111;
+
+	public static void main(String args[]){
+		ByteStreamTransformer transformer = new ServerTransformer(new NamingServiceMessageServer());
+		Address myAddr = new Entry(address,portNo);
+		Replyer r = new Replyer("NamingService", myAddr);
+		while (true) {
+			try {
+				r.receive_transform_and_send_feedback(transformer);
+			} catch (Exception e){
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+	//returns the port number the server was registered at
+	public static void registerMethod(String name, Object obj) {
+		String data = name + ":" + obj.getClass().getTypeName();
+		Message msg= new Message("Server",data);
+		Requestor req = new Requestor("Server");
+		Marshaller m = new Marshaller();
+		byte[] bytes = m.marshal(msg);
+		Address dest = new Entry(address,portNo);
+		bytes = req.deliver_and_wait_feedback(dest, bytes);
+		Message answer = m.unmarshal(bytes);
+		System.out.println("Registered object " + obj.getClass().getTypeName()+" with name "+name+" at "+answer.data);
+		//return Integer.parseInt(answer.data);
+		//Se creaza instanta lui serverside proxy
+		int portNumber = Integer.parseInt(answer.data);
+		InfoServerProxy proxy = new InfoServerProxy(portNumber);
+		//si se invoca metoda de start a acestuia
+		proxy.dispatch();
+
+	}
+
+	//returns the port number of the object the client searched for
+	public static Object getObjectReference(String name){
+		Message msg = new Message("Client",name);
+		Requestor req = new Requestor("Client");
+		Marshaller m = new Marshaller();
+		byte[] bytes = m.marshal(msg);
+		Address dest = new Entry(address,portNo);
+		bytes = req.deliver_and_wait_feedback(dest, bytes);
+		Message answer = m.unmarshal(bytes);
+		System.out.println("Object reference with name: " + name+" was found at port number: "+answer.data);
+		int retVal = Integer.parseInt(answer.data);
+		//se returneaza o referinta (o instanta a lui client side proxy de fapt)
+		//la obiectul server care a fost inregistrat ca “MyInfoImpl”
+		InfoClientProxy proxy = new InfoClientProxy(retVal);
+		return proxy;
+	}
+
+}
